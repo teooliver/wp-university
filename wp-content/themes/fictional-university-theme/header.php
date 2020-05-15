ac09fd712ea257933643ae95333a52201d19f4aa
Teo Oliver <teo.liz.oli@gmail.com>
2020-05-15 20:11:53 +0100

    added custom queries on front-pag

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo('charset') ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
   <title>Document</title>
 </head>
+
 <body <?php body_class(); ?>>
 
-<header class="site-header">
+  <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong>
+          University</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul>
-            <li <?php if( is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li <?php if( is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"' ?>><a
+                href="<?php echo site_url('/about-us') ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
+            <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>>
+              <a href="<?php echo site_url('/blog') ?>">Blog</a></li>
           </ul>
         </nav>
         <div class="site-header__util">
